00d1dc6590138247e4cc64f9d6f8b79f8575959a
LE402Nt\uer <oo800217@gmail.com>
2023-12-20 16:59:41 +0800

    換個分析方法

--- 20231220.R
+++ 20231220.R
@@ -32,3 +32,11 @@ colnames(Data)[3]
 
 Result1 = lm(Data[,"Weight"]~Data[,"Height"])
 summary(Result1)
+
+
+Result1 = cor.test(Data[,"Weight"], Data[,"Height"], method = "pearson") # Pearson correlation
+Result1
+
+
+Result2 = cor.test(Data[,"Weight"], Data[,"Height"], method = "spearman") # Spearman correlation
+Result2
